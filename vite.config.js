b2824147ff29980f142163ff207b6a431c8ef25b
Connor Close <cclose@shrevecrumpandlow.com>
2025-05-13 15:16:52 -0400

    v003

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/dco/', // 👈 required for GitLab Pages
+  base: '/dco/', // 👈 Required for GitHub Pages
   plugins: [react()],
 })
